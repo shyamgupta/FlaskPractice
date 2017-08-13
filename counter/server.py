60d5c9170a3b99321041c1068d6f40ba8c031d60
Shyam Gupta <postboxshyam@gmail.com>
2017-08-13 16:13:47 -0400

    Counter Assignment

--- counter/server.py
+++ counter/server.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template,session
 app = Flask(__name__)
 app.secret_key = '\x1bAp\x99\x0b\xbf\x10\xfd\x9b\x83\x0bY\xf2\x86\x92\xcbB\xe7\x98\xbd\x98\x85\xc7\x0b'
-counter = 0
+def visit_counter():
+	try:
+		session['counter'] += 1
+	except KeyError:
+		session['counter'] =1
 @app.route('/')
 def index():
-	counter += 1
-	return render_template('index.html',counter=counter)
-	
+	visit_counter()
+	return render_template('index.html',counter = session['counter'])
+@app.route('/bytwo',methods=['GET','POST'])
+def add2():
+	session['counter'] += 2
+	return render_template('index.html',counter = session['counter'])
+
+@app.route('/reset',methods=['GET','POST'])
+def reset():
+	session['counter'] =1
+	return render_template('index.html',counter = session['counter'])
 
 app.run(debug=True)
