46c3cb083f3d57287fc207daebc13721933ff14d
Shyam Gupta <postboxshyam@gmail.com>
2017-08-14 00:10:23 -0400

    Numbers Game

--- numbergame/server.py
+++ numbergame/server.py
@@ -1,31 +1,35 @@
 from flask import Flask,render_template,session,url_for, request,redirect
 import random
-import random
 app = Flask(__name__)
-app.secret_key = "HelloWorld"
+app.secret_key = "HelloWorldHowAreYou"
 
-@app.route('/' ,methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
 	return render_template('index.html')
 
 @app.route('/guess',methods=['POST'])
 def guess():
-	if 'rand' not in session:
-		session['rand'] = random.randint(1,101)
-	guess = int(request.form['guess'])
 	
-	if guess < session['rand']:
-		session['user_guess'] = 'Too Low'
-	elif guess > session['rand']:
-		session['user_guess'] = 'Too High'
-	else:
-		session['user_guess'] = 'Correct'
+	guess = int(request.form['guess'])
+	if 'number' not in session:
+		session['number'] = random.randint(1,100)
+	if guess < session['number']:
+		session['message'] = "Too Low"
+		session['color_name']='red'
+	elif guess>session['number']:
+		session['message'] = "Too high"
+		session['color_name'] = "red"
+	elif guess == session['number']:
+		session['message'] = "Right number!"
+		session['color_name'] = "green"
 	return redirect('/')
 
-@app.route('/reset', methods=['POST'])
+@app.route('/reset',methods=['POST'])
 def reset():
 	session.clear()
 	return redirect('/')
 
 
 app.run(debug=True)
+
+
