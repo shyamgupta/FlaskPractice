8a86742cbb17a6415f3401d186e3c27b373bc52b
Shyam Gupta <postboxshyam@gmail.com>
2017-08-16 19:55:38 -0400

    Form Validation

--- dojosurvey/server.py
+++ dojosurvey/server.py
@@ -1,15 +1,18 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,flash,url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'u\xaa9\xfa\xe5\xbbI\xc5\xef\xafXqUR\x94\xc7\x9a\x1e\xa9s[\xe3%\xec'
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
 def result():
-	name = request.form['name']
-	location = request.form['location']
-	language = request.form['language']
-	comment = request.form['comment']
-	return render_template('info.html',name=name,location=location, language=language,comment=comment)
-	
+	if len(request.form['name']) <1:
+		flash("Name cannot be blank!")
+	else:
+		flash("Success! You name is {}".format(request.form['name']))
+	if len(request.form['comment'])>120:
+		flash('Comments should be less than 120 characters')
+	return redirect('/')
+
 app.run(debug=True)
